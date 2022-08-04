9b996dd3505957d77409276d48414575294b7a6c
nishisuke <ryoskns+git@gmail.com>
2022-08-04 23:28:23 +0900

    relay

---
+++ src/fetchGraphQL.ts
@@ -0,0 +1,20 @@
+async function fetchGraphQL(text: string, variables: any) {
+  const REACT_APP_GITHUB_AUTH_TOKEN = process.env.REACT_APP_GITHUB_AUTH_TOKEN;
+
+  const response = await fetch('http://localhost:8080/query', {
+    method: 'POST',
+    headers: {
+      Authorization: `bearer ${REACT_APP_GITHUB_AUTH_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  });
+
+  // Get the response as JSON
+  return await response.json();
+}
+
+export default fetchGraphQL;
